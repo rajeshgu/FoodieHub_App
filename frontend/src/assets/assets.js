0cd3ea6c5df66c55dff88ebca1b0455fe7b8ceac
rajeshgu <rajeshlkh2020@gmail.com>
2025-08-21 02:15:36 +0530

    Add menu item list to Explore Menu section

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,7 +1,30 @@
 import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+// assets
+import rating_starts from './rating_starts.png'
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+
+// food list
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
 
 export const assets = {
-  menu_1
+  rating_starts,
+  add_icon_white,
+  add_icon_green,
+  remove_icon_red
 }
 
 // all menu name and images 
@@ -9,5 +32,74 @@ export const menu_list = [
   {
     menu_name: "Salad",
     menu_image: menu_1
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2
+  },
+  {
+    menu_name: "Deserts",
+    menu_image: menu_3
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4
+  },
+  {
+    menu_name: "Cake",
+    menu_image: menu_5
+  },
+  {
+    menu_name: "Pure Veg",
+    menu_image: menu_6
+  },
+  {
+    menu_name: "Pasta",
+    menu_image: menu_7
+  },
+  {
+    menu_name: "Noodles",
+    menu_image: menu_8
   }
 ]
+
+// all food list 
+export const food_list = [
+  {
+    _id: "1",
+    name: "Greek salad",
+    image: food_1,
+    price: 12,
+    description: "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  },
+  {
+    _id: "2",
+    name: "Veg salad",
+    image: food_2,
+    price: 18,
+    description: "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  }, {
+    _id: "3",
+    name: "Clover Salad",
+    image: food_3,
+    price: 16,
+    description: "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  }, {
+    _id: "4",
+    name: "Chicken Salad",
+    image: food_4,
+    price: 24,
+    description: "Food provides essential nutrients for overall health and well-being",
+    category: "Salad"
+  }, {
+    _id: "5",
+    name: "Lasagna Rolls",
+    image: food_5,
+    price: 14,
+    description: "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls"
+  }
+]   
