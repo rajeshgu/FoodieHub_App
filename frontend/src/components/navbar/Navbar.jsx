1efad4e1d3d9cefb491b67c41d08f66c1b2d5f50
rajeshgu <rajeshlkh2020@gmail.com>
2025-08-23 22:27:39 +0530

    Make app reponsibness

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,42 +3,56 @@ import "./Navbar.css";
 import logo from "../../assets/Logo.png";
 import searchIcon from "../../assets/search_icon.png";
 import basketIcon from "../../assets/basket_icon.png";
+import Hamburger from "hamburger-react";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("");
+  const [menu, setMenu] = useState("home");
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = (section) => {
+    setMenu(section);
+    setMenuOpen(false);
+  };
+
   return (
     <div className="navbar">
-      <img src={logo} alt="FoodieHub_App logo" className="logo" />
-      <ul className="navbar-menu">
+      <img src={logo} alt="FoodieHub Logo" className="logo" />
+
+      <div className="hamburger">
+        <Hamburger toggled={menuOpen} toggle={setMenuOpen} />
+      </div>
+
+      <ul className={`navbar-menu ${menuOpen ? "active" : ""}`}>
         <li
-          onClick={() => setMenu("home")}
+          onClick={() => handleMenuClick("home")}
           className={menu === "home" ? "active" : ""}
         >
           Home
         </li>
         <li
-          onClick={() => setMenu("menu")}
+          onClick={() => handleMenuClick("menu")}
           className={menu === "menu" ? "active" : ""}
         >
           Menu
         </li>
         <li
-          onClick={() => setMenu("mobile")}
-          className={menu === "mobile" ? "active" : "e-app"}
+          onClick={() => handleMenuClick("mobile")}
+          className={menu === "mobile" ? "active" : ""}
         >
           Mobile App
         </li>
         <li
-          onClick={() => setMenu("Contact-us")}
-          className={menu === "Contact-us" ? "active" : ""}
+          onClick={() => handleMenuClick("contact")}
+          className={menu === "contact" ? "active" : ""}
         >
           Contact Us
         </li>
       </ul>
+
       <div className="navbar-right">
-        <img src={searchIcon} alt="Search Icon" />
+        <img src={searchIcon} alt="Search" />
         <div className="navbar-search-icon">
-          <img src={basketIcon} alt="Basket Icon" />
+          <img src={basketIcon} alt="Basket" />
           <div className="dot"></div>
         </div>
         <button>Sign in</button>
